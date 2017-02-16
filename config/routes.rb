b8b9c5730948ed4c6b9209e949ac8b30c4afcfed
Charles Pernet <charlespernet@gmail.com>
2017-02-16 19:12:09 +0100

    fin livecode

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :cocktails, only: [:show, :index, :new, :create] do
+    resources :doses, only: [:create]
+  end
+  resources :doses, only: [:destroy]
+  root "cocktails#index"
+end
